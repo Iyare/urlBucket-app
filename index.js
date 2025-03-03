70190f9541a0b76b4f299ed02a1482f8fd54bfd6
Iyare Usen <iyare.usen@gmail.com>
2025-03-03 15:26:41 +0100

    refactors code to use firebase database instead of localstorage

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-const firebaseConfig = {};
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
+
+const firebaseConfig = {
+    databaseURL: "https://url-bucket-app-default-rtdb.firebaseio.com/"
+};
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const databaseReference = ref(database, "urls");
 
 // chrome://extensions/
 let myLeads = [];
 const inputEl = document.querySelector("#input-el");
-const inputBtn = document.querySelector("#input-btn");
+const saveBtn = document.querySelector("#save-btn");
 const deleteAllBtn = document.querySelector("#delete-btn");
-const tabBtn = document.querySelector("#tab-btn");
 const list = document.querySelector("#list");
 
-let savedLeads = JSON.parse(localStorage.getItem("myLeads"));
+
 
 // Checking if there are any saved URLs in the local storage
-if (savedLeads) {
-    myLeads = savedLeads;
-    renderLeads(myLeads);
-}
+// if (savedLeads) {
+//     myLeads = savedLeads;
+//     renderLeads(myLeads);
+// }
 
 // Adding event listener to input element to save the URL when Enter key is pressed
 inputEl.addEventListener("keypress", function (e) {
@@ -28,7 +33,7 @@ inputEl.addEventListener("keypress", function (e) {
 })
 
 // Adding event listener to save button to save the URL when clicked
-inputBtn.addEventListener("click", saveUrl)
+saveBtn.addEventListener("click", saveUrl)
 
 // Function to save the URL
 function saveUrl() {
@@ -41,17 +46,20 @@ function saveUrl() {
             url = "https://" + url;
         }
         myLeads.push(url);
-        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        push(databaseReference, url)
+        // localStorage.setItem("myLeads", JSON.stringify(myLeads));
         inputEl.value = "";
         renderLeads(myLeads);
     }
+ 
+    
 }
 
 // Function to render the list of URLs  in the list element
 
 function renderLeads(leads) {
     let listItems = ""
-    // Refactor the onclick function on the i element
+    //Note +++++  Refactor the onclick function on the i element
     for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
@@ -65,18 +73,11 @@ function renderLeads(leads) {
 // Adding event listener to delete all button to delete all the URLs saved
 deleteAllBtn.addEventListener("click", deleteAllLeads)
 function deleteAllLeads() {
-    localStorage.clear();
+    // localStorage.clear();
     myLeads = [];
     renderLeads(myLeads);
 }
 
-// Adding event listener to tab button to save the URL of the current tab
-tabBtn.addEventListener("click", function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        renderLeads(myLeads)
-    })
-
-})
-
+onValue(databaseReference, (snapshot) => { 
+    console.log(snapshot.value)
+}) 
