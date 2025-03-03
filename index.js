2f90aaba1ec7aa6b0e4b3628dac1cb5912b52023
Iyare Usen <iyare.usen@gmail.com>
2025-03-03 10:38:43 +0100

    refactors JS code

--- index.js
+++ index.js
@@ -10,73 +10,71 @@ let savedLeads = JSON.parse(localStorage.getItem("myLeads"));
 
 // Checking if there are any saved URLs in the local storage
 if (savedLeads) {
-    myLeads = savedLeads;
-    renderLeads(myLeads);
+  myLeads = savedLeads;
+  renderLeads(myLeads);
 }
 
 // Adding event listener to input element to save the URL when Enter key is pressed
 inputEl.addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-        saveUrl();
-    }
-})
+  if (e.key === "Enter") {
+    saveUrl();
+  }
+});
 
 // Adding event listener to save button to save the URL when clicked
-inputBtn.addEventListener("click", saveUrl)
+inputBtn.addEventListener("click", saveUrl);
 
 // Function to save the URL
 function saveUrl() {
-    let url = inputEl.value;
-    if (!url) {
-        alert("Please enter a valid URL")
-        return
-    } else {
-        if (!url.includes("http")) {
-            url = "https://" + url;
-        }
-        myLeads.push(url);
-        localStorage.setItem("myLeads", JSON.stringify(myLeads));
-        inputEl.value = "";
-        renderLeads(myLeads);
+  let url = inputEl.value;
+  if (!url) {
+    alert("Please enter a valid URL");
+    return;
+  } else {
+    if (!url.includes("http")) {
+      url = "https://" + url;
     }
+    myLeads.push(url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    inputEl.value = "";
+    renderLeads(myLeads);
+  }
 }
 
 // Function to render the list of URLs  in the list element
 
 function renderLeads(leads) {
-    let listItems = ""
-    // Refactor the onclick function on the i element
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
+  let listItems = "";
+  // Refactor the onclick function on the i element
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
             <li>
-                <a target='_blank' href='${leads[i]}'>${leads[i]}</a> 
-               
                 <i class="fas fa-trash-alt" 
                 onclick="
                 myLeads.splice(${i}, 1); 
                 localStorage.setItem('myLeads', JSON.stringify(myLeads)); renderLeads(myLeads)">
                 </i>
+            <a target='_blank' href='${leads[i]}'>${leads[i]}</a> 
+                       
             </li>
-        `
-    }
-    list.innerHTML = listItems  
+        `;
+  }
+  list.innerHTML = listItems;
 }
 
 // Adding event listener to delete all button to delete all the URLs saved
-deleteAllBtn.addEventListener("click", deleteAllLeads)
+deleteAllBtn.addEventListener("click", deleteAllLeads);
 function deleteAllLeads() {
-    localStorage.clear();
-    myLeads = [];
-    renderLeads(myLeads);
+  localStorage.clear();
+  myLeads = [];
+  renderLeads(myLeads);
 }
 
 // Adding event listener to tab button to save the URL of the current tab
-tabBtn.addEventListener("click", function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        renderLeads(myLeads)
-    })
-
-})
-
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads(myLeads);
+  });
+});
