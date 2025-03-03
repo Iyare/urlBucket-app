19c1df4c9b6de6cd89b9bc3ac14d29c209a3bdb5
Iyare Usen <iyare.usen@gmail.com>
2025-03-03 20:04:08 +0100

    refactors delete code  to remove database reference

--- index.js
+++ index.js
@@ -1,83 +1,91 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
 
 const firebaseConfig = {
-    databaseURL: "https://url-bucket-app-default-rtdb.firebaseio.com/"
+  databaseURL: "https://url-bucket-app-default-rtdb.firebaseio.com/",
 };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const databaseReference = ref(database, "urls");
 
-// chrome://extensions/
-let myLeads = [];
+
+let myUrls = [];
 const inputEl = document.querySelector("#input-el");
 const saveBtn = document.querySelector("#save-btn");
 const deleteAllBtn = document.querySelector("#delete-btn");
 const list = document.querySelector("#list");
 
-
-
 // Checking if there are any saved URLs in the local storage
 // if (savedLeads) {
-//     myLeads = savedLeads;
-//     renderLeads(myLeads);
+//     myUrls = savedLeads;
+//     renderLeads(myUrls);
 // }
 
 // Adding event listener to input element to save the URL when Enter key is pressed
 inputEl.addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-        saveUrl();
-    }
-})
+  if (e.key === "Enter") {
+    saveUrl();
+  }
+});
 
 // Adding event listener to save button to save the URL when clicked
-saveBtn.addEventListener("click", saveUrl)
+saveBtn.addEventListener("click", saveUrl);
 
 // Function to save the URL
 function saveUrl() {
-    let url = inputEl.value;
-    if (!url) {
-        alert("Please enter a valid URL")
-        return
-    } else {
-        if (!url.includes("http")) {
-            url = "https://" + url;
-        }
-        myLeads.push(url);
-        push(databaseReference, url)
-        // localStorage.setItem("myLeads", JSON.stringify(myLeads));
-        inputEl.value = "";
-        renderLeads(myLeads);
+  let url = inputEl.value;
+  if (!url) {
+    alert("Please enter a valid URL");
+    return;
+  } else {
+    if (!url.includes("http")) {
+      url = "https://" + url;
     }
- 
-    
+    myUrls.push(url);
+    push(databaseReference, url);
+    // localStorage.setItem("myUrls", JSON.stringify(myUrls));
+    inputEl.value = "";
+    renderLeads(myUrls);
+  }
 }
 
 // Function to render the list of URLs  in the list element
 
-function renderLeads(leads) {
-    let listItems = ""
-    //Note +++++  Refactor the onclick function on the i element
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
+function renderLeads(urls) {
+  let listItems = "";
+  //Note +++++  Refactor the onclick function on the i element
+  for (let i = 0; i < urls.length; i++) {
+    listItems += `
             <li>
-                <a target='_blank' href='${leads[i]}'>${leads[i]}</a> 
+                <a target='_blank' href='${urls[i]}'>${urls[i]}</a> 
             </li>
-        `
-    }
-    list.innerHTML = listItems  
+        `;
+  }
+  list.innerHTML = listItems;
 }
 
 // Adding event listener to delete all button to delete all the URLs saved
-deleteAllBtn.addEventListener("click", deleteAllLeads)
+deleteAllBtn.addEventListener("click", deleteAllLeads);
 function deleteAllLeads() {
-    // localStorage.clear();
-    myLeads = [];
-    renderLeads(myLeads);
+  // localStorage.clear();
+  remove(databaseReference);
+  list.innerHTML = ""
+  myUrls = [];
+  // renderLeads(myUrls);
 }
 
-onValue(databaseReference, (snapshot) => { 
-    console.log(snapshot.value)
-}) 
+onValue(databaseReference, (snapshot) => {
+  const isContentinDatabase = snapshot.exists();
+  if (isContentinDatabase) {
+    const snapshotValues = snapshot.val();
+  myUrls = Object.values(snapshotValues);
+  renderLeads(myUrls);
+  }
+});
